4a72e5cb785c914002e5dc1d9b1968c388cb8df9
Bixia Deng <bixiad@uci.edu>
2020-01-11 21:35:24 -0800

    Initial Commit

---
+++ Tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip
+//
+//  Created by Kaylee Deng on 1/11/20.
+//  Copyright © 2020 Kaylee Deng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
